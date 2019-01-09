1d2a9dc81c7bdf09406166d518cdcb54d216198e
Axel GINEPRO <axelginepro@gmail.com>
2019-01-09 16:35:22 +0100

    bikeshop 1st

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 // var dataCardBike =[];
+var dataBike = [
+  {name: "Model GUIB45", url:"/images/bike-1.jpg", price: 679},
+  {name: "Model ZOOK7", url:"/images/bike-2.jpg", price: 799},
+  {name: "Model UYAR89", url:"/images/bike-3.jpg", price: 839},
+  {name: "Model VERS77", url:"/images/bike-4.jpg", price: 1299},
+  {name: "Model TITAN5", url:"/images/bike-5.jpg", price: 559},
+  {name: "Model GINE39", url:"/images/bike-6.jpg", price: 999}
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var dataBike = [
-    {name: "Model BIKO45", url:"/images/bike-1.jpg", price: 679},
-    {name: "Model ZOOK7", url:"/images/bike-2.jpg", price: 799},
-    {name: "Model LIKO89", url:"/images/bike-3.jpg", price: 839},
-    {name: "Model GEWO", url:"/images/bike-4.jpg", price: 1206},
-    {name: "Model TITAN5", url:"/images/bike-5.jpg", price: 989},
-    {name: "Model AMIG39", url:"/images/bike-6.jpg", price: 599}
-  ]
 
   if (!req.session.dataCardBike) {
     req.session.dataCardBike = [];
@@ -61,5 +61,28 @@ router.post('/update-shop', function(req, res, next) {
 
   res.render('shop', {dataCardBike: req.session.dataCardBike});
 });
+
+
+router.post('/checkout', function(req, res) {
+
+  var stripe = require("stripe")("pk_test_vBeV2C4ImP7ljsJN5BGQINgB");
+  const token = req.body.stripeToken; // Using Express
+  const charge = stripe.charges.create({
+    amount: req.body.totalCmd,
+    currency: 'eur',
+    description: 'Example charge',
+    source: token,
+  });
+  req.session.overallCmd = req.session.dataCardBike;
+  req.session.totalAmount = req.body.totalCmd / 100;
+  req.session.dataCardBike = [];
+  res.render('checkout', {overallCmd: req.session.overallCmd, totalAmount: req.session.totalAmount});
+});
+
+
+router.post('/cleancard', function(req, res) {
+  req.session.dataCardBike = [];
+  res.render('shop', {dataCardBike: req.session.dataCardBike});
+})
 
 module.exports = router;
